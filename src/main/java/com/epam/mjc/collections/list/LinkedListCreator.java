648a1bb8d67b7c3867807ef6ad51bdf1c3d32ef5
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-20 14:12:08 +0500

    finished the task

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> answer = new LinkedList<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                answer.addLast(num);
+            } else {
+                answer.addFirst(num);
+            }
+        }
+
+        return answer;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        long first = Long.parseLong(a),
+                second = Long.parseLong(b),
+                firstMod = Math.abs(first),
+                secondMod = Math.abs(second);
+        int result = Long.compare(firstMod, secondMod);
+
+        return result == 0 ? Long.compare(first, second) : result;
     }
 }
